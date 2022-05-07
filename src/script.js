c6c03cf7411da8fec5baceaf9024ea08be7e71ed
lauchRocke <launchspacerocket@yahoo.com>
2022-05-07 09:03:34 -0400

    added create link

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const create = document.querySelector(".create");
+const textarea = document.querySelector("#textarea");
+const button = document.querySelector(".button");
+const text = document.querySelector(".text");
+const write = document.querySelector("#write")
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log(textarea.value)
+})
+text.innerHTML = form.value;
