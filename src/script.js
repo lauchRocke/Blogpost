2587a9551b8430acca36b0a2f4004287bd9f3a37
lauchRocke <launchspacerocket@yahoo.com>
2022-05-09 23:13:22 -0400

    fix css bug

--- src/script.js
+++ src/script.js
@@ -4,9 +4,13 @@ const button = document.querySelector(".button");
 const text = document.querySelector(".text");
 const write = document.querySelector("#write")
 const form = document.querySelector("#form");
+const paragraph_container = document.querySelector(".paragraph_container")
 
-form.addEventListener("submit", function(e){
+button.addEventListener("click", function(e){
     e.preventDefault();
-    console.log(textarea.value)
+    console.log(textarea.value);
+    let paragraph = document.createElement('p');
+    paragraph.innerText = textarea.value;
+    // paragraph_container.appendChild(paragraph);
+
 })
-text.innerHTML = textarea.value;
